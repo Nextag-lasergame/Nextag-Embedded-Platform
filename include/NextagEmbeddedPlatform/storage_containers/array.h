8f65b6df0990b7da507f7fdce30f35a7519a1513
Tim Herreijgers <timtim3001@gmail.com>
2023-05-23 08:26:02 +0200

    Added initial array header

---
+++ include/NextagEmbeddedPlatform/storage_containers/array.h
@@ -0,0 +1,13 @@
+/*
+* Copyright © 2022 Tim Herreijgers
+* Licensed using the MIT license
+*/
+
+#pragma once
+
+namespace NextagEmbeddedPlatform::StorageContainers
+{
+
+
+
+} // namespace NextagEmbeddedPlatform::Drivers
