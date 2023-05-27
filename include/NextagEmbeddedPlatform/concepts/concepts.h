bed62f4269400f42c3f753ef642bdfaa4253c9c3
Tim Herreijgers <timtim3001@gmail.com>
2023-05-27 20:19:08 +0200

    Finished documentation for circular_buffer

---
+++ include/NextagEmbeddedPlatform/concepts/concepts.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright © 2022 Tim Herreijgers
+ * Licensed using the MIT license
+ */
+
+#pragma once
+
+namespace NextagEmbeddedPlatform::Concepts
+{
+
+template<typename T>
+concept is_default_constructable = requires(T t)
+{
+    T{};
+};
+
+} // namespace NextagEmbeddedPlatform::Concepts
