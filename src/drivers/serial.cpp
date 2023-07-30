db75460d61ac698b938fe038b69ded507d383cbc
Tim Herreijgers <timtim3001@gmail.com>
2023-07-30 22:41:02 +0200

    NEXTAG-20 Added empty Serial class

---
+++ src/drivers/serial.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright © 2022 Tim Herreijgers
+ * Licensed using the MIT license
+ */
+
+#include "NextagEmbeddedPlatform/drivers/serial.h"
+
+namespace NextagEmbeddedPlatform::Drivers
+{
+
+} // namespace NextagEmbeddedPlatform::Drivers
