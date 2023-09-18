11527669a6c70688a91aad76e565d5d7d94fea0a
Tim Herreijgers <timtim3001@gmail.com>
2023-09-18 20:11:17 +0200

    NEXTAG-26 Added implentation for setMode

--- src/drivers/timer.cpp
+++ src/drivers/timer.cpp
@@ -3,17 +3,33 @@
 * Licensed using the MIT license
  */
 
-#pragma once
-
 #include "NextagEmbeddedPlatform/drivers/timer.h"
+
+#include "timer_registers.h"
+
+#include <avr/io.h>
 
 namespace NextagEmbeddedPlatform::Drivers
 {
 
-Timer Timer::timer0{};
-
-void Timer::setMode(TimerMode mode)
+class Timer8Bit : public Timer
 {
-}
+public:
+    explicit Timer8Bit(TimerRegister<uint8_t> * registers) : m_registers(registers)
+    {
+
+    }
+
+    void setMode(TimerMode /*mode*/) override
+    {
+        m_registers->controlB = m_registers->controlB | _BV(WGM01);
+    }
+
+private:
+    TimerRegister<uint8_t> * m_registers;
+};
+
+static Timer8Bit timer0Definition{reinterpret_cast<TimerRegister<uint8_t>*>(0x44)};
+Timer & Timer::timer0 = timer0Definition;
 
 } // namespace NextagEmbeddedPlatform::Drivers
