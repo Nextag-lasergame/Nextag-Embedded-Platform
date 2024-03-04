f6cc835f0dcaa34d5d50361d89420c450659de57
Tim Herreijgers <timtim3001@gmail.com>
2024-03-04 12:29:22 +0000

    Updated setup_codespace.sh

--- setup_codespace.sh
+++ setup_codespace.sh
@@ -1,16 +1,18 @@
 mkdir avr-gcc
 cd avr-gcc 
-wget https://github.com/ZakKemble/avr-gcc-build/releases/download/v12.1.0-1/avr-gcc-12.1.0-x64-linux.tar.bz2
-tar -xf avr-gcc-12.1.0-x64-linux.tar.bz2
-echo export PATH=/workspaces/Nextag-Embedded-Platform/avr-gcc/avr-gcc-12.1.0-x64-linux/bin:$PATH > ~/.bashrc
+wget https://github.com/Nextag-lasergame/avr-gcc-toolchains/releases/download/v13.2/avr-gcc-13.2.0-x64-linux.tar.gz
+tar -xf avr-gcc-13.2.0-x64-linux.tar.gz
+echo export PATH=/workspaces/Nextag-Embedded-Platform/avr-gcc/avr-gcc-13.2.0-x64-linux/bin:$PATH > ~/.bashrc
 cd ..
 
 sudo wget -qO /usr/local/bin/ninja.gz https://github.com/ninja-build/ninja/releases/latest/download/ninja-linux.zip
 sudo gunzip /usr/local/bin/ninja.gz
 sudo chmod a+x /usr/local/bin/ninja
 
+wget -qO - https://apt.kitware.com/keys/kitware-archive-latest.asc | sudo apt-key add -
+sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
 sudo apt update
-sudo apt install simavr -y
+sudo apt install simavr cmake -y
 
 source ~/.bashrc
 
